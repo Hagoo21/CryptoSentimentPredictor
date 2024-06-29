cfc12c987ca18a5660e8acba126133c862facc3a
DESKTOP-FOTQ5RK\shaya <rahman@myteabot.com>
2024-06-29 14:24:27 -0400

    Flask and React project template code

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react';
+
+const ComponentName = () => {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default ComponentName;
